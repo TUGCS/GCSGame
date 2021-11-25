fa5f57ac2abe485ec313a28be160938daf3bbcc8
Justinas Grigas <jstn_as@protonmail.com>
2021-11-25 09:26:11 +0000

    working on car controller

--- Assets/Scripts/Justinas/CarController.cs
+++ Assets/Scripts/Justinas/CarController.cs
@@ -1,18 +1,83 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class CarController : MonoBehaviour
+namespace Justinas
 {
-    // Start is called before the first frame update
-    void Start()
+    public class CarController : MonoBehaviour
     {
-        
-    }
+        [SerializeField] private float acceleration, deceleration, steering;
+        [SerializeField] private InputActionReference accelerateAction, brakeAction, handBrakeAction, steerAction;
+        private Rigidbody _rb;
+        private bool _accelerating, _braking, _handBraking;
+        private float _steer;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private void Awake()
+        {
+            _rb = GetComponent<Rigidbody>();
+            accelerateAction.action.performed += OnAccelerate;
+            accelerateAction.action.canceled += OnAccelerate;
+            brakeAction.action.performed += OnBreak;
+            brakeAction.action.canceled += OnBreak;
+            handBrakeAction.action.performed += OnHandBrake;
+            handBrakeAction.action.canceled += OnHandBrake;
+            steerAction.action.performed += OnSteer;
+            steerAction.action.canceled += OnSteer;
+        }
+
+        private void OnEnable()
+        {
+            accelerateAction.action.Enable();
+            brakeAction.action.Enable();
+            handBrakeAction.action.Enable();
+            steerAction.action.Enable();
+        }
+
+        private void OnDisable()
+        {
+            accelerateAction.action.Disable();
+            brakeAction.action.Disable();
+            handBrakeAction.action.Disable();
+            steerAction.action.Disable();
+        }
+
+        private void FixedUpdate()
+        {
+            var force = Vector3.zero;
+            var torque = Vector3.zero;
+            var t = transform;
+            if (_accelerating)
+            {
+                torque += t.up * steering * _steer;
+                force += t.forward * acceleration;
+            }
+            if (_braking)
+            {
+                torque -= t.up * steering * _steer;
+                force -= t.forward * deceleration;
+            }
+            _rb.AddTorque(torque);
+            _rb.AddForce(force);
+        }
+
+        private void OnAccelerate(InputAction.CallbackContext obj)
+        {
+            _accelerating = obj.performed;
+        }
+
+        private void OnBreak(InputAction.CallbackContext obj)
+        {
+            _braking = obj.performed;
+        }
+
+        private void OnHandBrake(InputAction.CallbackContext obj)
+        {
+            _handBraking = obj.performed;
+        }
+
+        private void OnSteer(InputAction.CallbackContext obj)
+        {
+            _steer = obj.ReadValue<float>();
+        }
     }
 }
